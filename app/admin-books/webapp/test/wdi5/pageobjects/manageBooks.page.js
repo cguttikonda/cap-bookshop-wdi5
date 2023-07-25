3c161a95f74ce1dff62973378727ec9a2e6e8de3
Chankya Guttikonda <chanakya.guttikonda@henkel.com>
2023-07-25 14:12:53 +0000

    Test cases

--- app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
@@ -2,7 +2,28 @@ module.exports = {
   /**
     * define actions for the page object
   */
+  iClickOnCreateNewBook: async () => {
+    await browser.asControl({
+    	selector: {
+            id: "books::BooksList--fe::table::Books::LineItem::StandardAction::Create",
+            interaction: {
+                    idSuffix: "BDI-content"
+            }
+    }}).press();
+    
+},
+theBookListContains: async (sBook) => {
+  const visible = await browser.asControl({
+    selector: {
+          controlType: "sap.m.Text",
+          viewId: "books::BooksList",
+          properties: {
+                  text: sBook
+          }
+  }}).getText();
+  expect(text).toEqual(sBook);
 
+}
   /**
    * define assertions for the page object
    */
--- app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
@@ -2,6 +2,76 @@ module.exports = {
         /**
          * define actions for the page object
          */
+        iEnterTitle: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::title::Field-edit",
+                        interaction: {
+                                idSuffix: "inner"
+                        }
+                }}).enterText("test");
+            },
+        
+            iSelectGenre: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::genre_ID::Field-edit-inner-vhi"
+                }}).press();
+                await browser.asControl({
+                        selector: {
+                        controlType: "sap.m.Text",
+                        viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+                        viewId: "books::BooksDetailsList",
+                        bindingPath: {
+                                path: "/Genres(10)",
+                                propertyPath: "name"
+                        },
+                        searchOpenDialogs: true
+                }}).press();
+            },
+            iSelectAuthor: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::author_ID::Field-edit-inner-vhi"
+                }}).press();
+        
+                await browser.asControl({
+                        selector: {
+                        controlType: "sap.m.Link",
+                        viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+                        viewId: "books::BooksDetailsList",
+                        bindingPath: {
+                                path: "/Authors(101)",
+                                propertyPath: "name"
+                        },
+                        searchOpenDialogs: true
+                }}).press();
+                    
+            },
+            iPressCreate: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::FooterBar::StandardAction::Save",
+                        interaction: {
+                                idSuffix: "BDI-content"
+                        }
+                }}).press();
+            },
+            iSeeEditButton: async () => {
+                const enabled = await browser.asControl({
+                        selector: {
+                        id: "books::BooksDetailsList--fe::StandardAction::Edit"
+                }}).getEnabled();
+                expect(enabled).toBeTruthy();
+            },
+            iNavigateBack: async () => {
+                await browser.asControl({
+                        selector: {
+                        id: "backBtn"
+                }}).press();
+            },
+            
+
 
         /**
          * define assertions for the page object
